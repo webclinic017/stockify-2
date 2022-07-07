23cfe70d0f58053597cf259301c09078b0761b54
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-07-07 20:04:05 +0400

    Update stocksite.py

--- stocksite.py
+++ stocksite.py
@@ -667,7 +667,7 @@ if dashboard=='Tradelyne':
 3. Backtesting - Backtesting answer's your " What if ? " question as to what if you had used xyz strategy on a stock over a period of time. Would you make profits or would it be a loss? Find out using our pre defined strategies in backtesting module.\n''')
 #st.write('''**Utilize these features by using the sidebar by opening the menu on mobile.**\n''')
 if dashboard=='Screener':
-    start_date = st.sidebar.date_input("Start date", datetime.date('2019', '1', '1'))
+    start_date = st.sidebar.date_input("Start date", datetime.date(2019, 1, 1))
     end_date = st.sidebar.date_input("End date", datetime.date(2021, 1, 31))
 
     # Retrieving tickers data
